b0630276f5b8368434b287f6b1f10bfef81cead1
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2023-04-29 00:41:01 +0200

    [feat]: create role data

--- src/pages/admin-panel/role/role/create-role.tsx
+++ src/pages/admin-panel/role/role/create-role.tsx
@@ -1,17 +1,33 @@
 import React, { useEffect, useState } from 'react'
 import * as ReactDOM from 'react-dom';
+import service from '../../../../services/admin.service';
+import { notifyError, notifySuccess } from '../../../../utils/alerts';
 
 const CreateRole = ({ showModal, onClose }: { showModal: Boolean, onClose: any }) => {
     const [isBrowser, setBrowser] = useState<Boolean>(false)
+    const [formData, setFormData] = useState({
+        value: '',
+        fee: 0
+    })
     useEffect(() => {
         setBrowser(true)
     }, []);
+    
     const handleOnSave = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         try {
-            
+            let res = await service.createRole(formData);
+            if (res) {
+                notifySuccess("Successfully Created the service");
+                setFormData({
+                    value: '',
+                    fee: 0
+                });
+                onClose();
+        
+            }
         } catch (error: any) {
-            console.log(error)
+            notifyError(error)
             return;
         }
     }
@@ -31,17 +47,13 @@ const CreateRole = ({ showModal, onClose }: { showModal: Boolean, onClose: any }
                         <label className="block text-gray-700 text-[12px] py-2">
                             Name
                         </label>
-                        <input className="shadow appearance-none bg-inputG border rounded w-full py-3 text-[10px] px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Enter role name" />
+                        <input value={formData.value} onChange={(e) => { setFormData({ ...formData, value: e.target.value }) }} className="shadow appearance-none bg-inputG border rounded w-full py-3 text-[10px] px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Enter role name" />
                     </div>
                     <div>
                         <label className="block text-gray-700 text-[12px] py-2">
-                            Status
+                            Fee
                         </label>
-                        <select className="shadow appearance-none bg-inputG border rounded w-full py-3 text-[10px] px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
-                            <option value={'none'}>Choose Status </option>
-                            <option value={'active'}>Active </option>
-                            <option value={'in-active'}>Inactive </option>
-                        </select>
+                        <input value={formData.fee} onChange={(e) => { setFormData({ ...formData, fee: e.target.valueAsNumber }) }} className="shadow appearance-none bg-inputG border rounded w-full py-3 text-[10px] px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="number" placeholder="Enter fee" />
                     </div>
                     <div>
                         <small className='text-[8px] py-1 text-red-500'>Enter Valid info</small>
